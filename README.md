549c383d7a1d2d2d63c8206b50c3443e9ea09320
Jimmy Thomas <bob@raccourci.fr>
2014-09-29 15:31:03 +0200

    Added some options Added mongo cache plugin

--- README.md
+++ README.md
@@ -261,3 +261,18 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+
+cookiesEnabled
+followRedirect
+isMaster
+iterations
+jsCheckTimeout
+jsTimeout
+messageTimeout
+noJsExecutionTimeout
+pageDoneCheckTimeout
+phantomArguments
+phantomBasePort
+resourceDownloadTimeout
+waitAfterLastRequest
+workers
--- server.js
+++ server.js
@@ -2,19 +2,29 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+  waitAfterLastRequest: 3 * 1000,
+  jsTimeout: 10 * 1000,
+  port: 4000,
+  noJsExecutionTimeout: 5 * 1000,
+  workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+  iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+  phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+  messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
+  phantomArguments: [
+    "--load-images=false",
+    "--ignore-ssl-errors=true",
+    "--disk-cache=false"
+  ]
 });
 
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(prerender.mongoDBHtmlCache());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
