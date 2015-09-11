0c218841076ea23cf95e3c536831d04d421eae08
Léo POIROUX <leo.poiroux@gmail.com>
2015-09-11 15:47:54 +0200

    Custom raccourci

--- server.js
+++ server.js
@@ -5,17 +5,32 @@ var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-});
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
+    accessLog: {
+        // Check out the file-stream-rotator docs for parameters
+        fileStreamRotator: {
+            filename: '/home/admin/logs/prerender/access-%DATE%.log',
+            frequency: 'daily',
+            date_format: 'YYYY-MM-DD',
+            verbose: false
+        },
 
+        // Check out the morgan docs for the available formats
+        morgan: {
+            format: 'combined'
+        }
+    }
+  });
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-server.use(prerender.blacklist());
+// server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
+server.use(require('prerender-access-log'));
+server.use(require('prerender-mongodb-cache'));
 
 server.start();
